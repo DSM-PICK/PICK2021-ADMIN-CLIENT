a3780591ca0aee0db3ff7c78177254094c764a4c
oajh04 <cloudtubekr@naver.com>
2021-09-05 13:29:45 +0900

    ⚡️ :: (#6) header style 추가

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled';
+
+export const HeaderWrapper = styled.div`
+    width: 280px;
+    margin: 0 auto;
+    margin-top: 40px;
+    margin-bottom: 32px;
+    img {
+        width: 100%;
+        height: 62px;
+    }
+    div {
+        font-family: Inter;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 24px;
+        line-height: 29px;
+        text-align: center;
+    }
+`
