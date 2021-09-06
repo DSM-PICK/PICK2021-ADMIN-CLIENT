93e87cb0ec08fd4222fb94420c6197b4ce980d21
oajh04 <cloudtubekr@naver.com>
2021-09-06 10:13:11 +0900

    ⚡️ :: (#10) home style

---
+++ src/components/Home/styles.ts
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+import { color } from '../../style/color';
+
+export const HomeWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    margin: 0 auto;
+`
+
+export const Line = styled.div`
+    width: 0;
+    height: 585px;
+    border: 1px solid #000000;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-45%);
+`
