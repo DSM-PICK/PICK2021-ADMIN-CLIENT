6498ec34e2457396d53c04761672c8a126f075b5
oajh04 <cloudtubekr@naver.com>
2021-09-06 10:13:51 +0900

    ⚡️ :: (#10) HomeRight

---
+++ src/components/Home/HomeRight/HomeRight.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import * as S from './styles'
+
+const HomeRight = () => {
+  return (
+    <>
+        <S.HomeRightWrapper>
+            
+        </S.HomeRightWrapper>
+    </>
+  );
+}
+
+export default HomeRight;
