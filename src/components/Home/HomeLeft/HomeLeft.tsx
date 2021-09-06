5fdceea84874ae4f8aba8b02612bad6d4b3a16e0
oajh04 <cloudtubekr@naver.com>
2021-09-06 10:13:28 +0900

    ⚡️ :: (#10) HomeLeft

---
+++ src/components/Home/HomeLeft/HomeLeft.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import * as S from './styles'
+
+const HomeLeft = () => {
+  return (
+    <>
+        <S.HomeLeftWrapper>
+            
+        </S.HomeLeftWrapper>
+    </>
+  );
+}
+
+export default HomeLeft;
