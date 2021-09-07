16d13a7ae83a124a0fb48f946a2e3a80b82fb2d8
oajh04 <cloudtubekr@naver.com>
2021-09-07 10:24:40 +0900

    ⚡️ :: (#10) HomeRight

--- src/components/Home/HomeRight/HomeRight.tsx
+++ src/components/Home/HomeRight/HomeRight.tsx
@@ -5,7 +5,8 @@ const HomeRight = () => {
   return (
     <>
         <S.HomeRightWrapper>
-            
+            <S.Title>학생 변동</S.Title>
+            <S.NoData>최근 변동사항이<br/>없습니다</S.NoData>
         </S.HomeRightWrapper>
     </>
   );
