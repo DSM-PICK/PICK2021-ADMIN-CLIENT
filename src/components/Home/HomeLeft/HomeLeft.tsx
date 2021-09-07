019994993e15ea65aa0fc249925cba1d58f83884
oajh04 <cloudtubekr@naver.com>
2021-09-07 10:24:19 +0900

    ⚡️ :: (#01) HomeLeft

--- src/components/Home/HomeLeft/HomeLeft.tsx
+++ src/components/Home/HomeLeft/HomeLeft.tsx
@@ -5,7 +5,8 @@ const HomeLeft = () => {
   return (
     <>
         <S.HomeLeftWrapper>
-            
+            <S.Title>동아리 변동</S.Title>
+            <S.NoData>최근 변동사항이<br/>없습니다</S.NoData>
         </S.HomeLeftWrapper>
     </>
   );
