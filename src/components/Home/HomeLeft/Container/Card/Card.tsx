d4d046925df73b5020f3d712ee97380d8ee7c775
oajh04 <cloudtubekr@naver.com>
2021-09-08 22:05:00 +0900

    ⚡️ :: (#10) HomeLeft Card

---
+++ src/components/Home/HomeLeft/Container/Card/Card.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import * as S from './styles';
+
+const Card = () => {
+  return (
+    <>
+        <S.CardWrapper>
+            <div>SEMICOLON;</div>
+            <div>추가</div>
+            <div>2021-09-12</div>
+            <div>한준호</div>
+        </S.CardWrapper>
+    </>
+  );
+}
+
+export default Card;
