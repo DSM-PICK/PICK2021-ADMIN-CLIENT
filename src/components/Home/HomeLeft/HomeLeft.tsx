9fe78cbd819405fc427395351938082201eb4536
oajh04 <cloudtubekr@naver.com>
2021-09-08 22:04:01 +0900

    ⚡️ :: (#10) HomeLeft

--- src/components/Home/HomeLeft/HomeLeft.tsx
+++ src/components/Home/HomeLeft/HomeLeft.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import LeftContainer from './Container/LeftContainer';
 import * as S from './styles'
 
 const HomeLeft = () => {
@@ -6,7 +7,8 @@ const HomeLeft = () => {
     <>
         <S.HomeLeftWrapper>
             <S.Title>동아리 변동</S.Title>
-            <S.NoData>최근 변동사항이<br/>없습니다</S.NoData>
+            {/* <S.NoData>최근 변동사항이<br/>없습니다</S.NoData> */}
+            <LeftContainer />
         </S.HomeLeftWrapper>
     </>
   );
