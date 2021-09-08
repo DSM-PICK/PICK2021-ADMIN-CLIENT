b6fd83b7ba07c35d31693bbbb209d403d570a394
oajh04 <cloudtubekr@naver.com>
2021-09-08 22:03:38 +0900

    ⚡️ :: (#10) HomeRight Card style

---
+++ src/components/Home/HomeRight/Container/Card/styles.ts
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export const CardWrapper = styled.div`
+    width: 500px;
+    margin: 0 auto;
+    margin-bottom: 8px;
+    display: grid;
+    text-align: center;
+    grid-template-columns: 95px 205px 105px 95px;
+`
