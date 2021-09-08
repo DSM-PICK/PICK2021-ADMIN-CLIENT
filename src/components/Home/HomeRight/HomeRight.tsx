41a310ce72e7ce7e7dfe2f6ad48371a7bbeba238
oajh04 <cloudtubekr@naver.com>
2021-09-08 22:02:38 +0900

    ⚡️ :: (#10) HomeRight

--- src/components/Home/HomeRight/HomeRight.tsx
+++ src/components/Home/HomeRight/HomeRight.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import RightContainer from './Container/RightContainer';
 import * as S from './styles'
 
 const HomeRight = () => {
@@ -6,7 +7,8 @@ const HomeRight = () => {
     <>
         <S.HomeRightWrapper>
             <S.Title>학생 변동</S.Title>
-            <S.NoData>최근 변동사항이<br/>없습니다</S.NoData>
+            {/* <S.NoData>최근 변동사항이<br/>없습니다</S.NoData> */}
+            <RightContainer/>
         </S.HomeRightWrapper>
     </>
   );
