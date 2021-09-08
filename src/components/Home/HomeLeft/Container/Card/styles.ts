1757c1144acec466e945a0eca9bcf0bc183bd2f8
oajh04 <cloudtubekr@naver.com>
2021-09-08 22:04:49 +0900

    ⚡️ :: (#10) HomeLeft Card style

---
+++ src/components/Home/HomeLeft/Container/Card/styles.ts
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export const CardWrapper = styled.div`
+    width: 500px;
+    margin: 0 auto;
+    margin-bottom: 8px;
+    display: grid;
+    text-align: center;
+    grid-template-columns: 125px 125px 125px 125px;
+`
