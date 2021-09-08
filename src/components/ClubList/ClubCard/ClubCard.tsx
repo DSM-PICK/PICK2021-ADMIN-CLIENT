20af817234655f75a0ab8d84fd9023a5683227c0
oajh04 <cloudtubekr@naver.com>
2021-09-08 22:23:10 +0900

    ⚡️ :: (#13) ClubCard

---
+++ src/components/ClubList/ClubCard/ClubCard.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import * as S from './styles'
+
+const ClubCard = () => {
+  return (
+    <>
+        <S.ClubCardWrapper>
+            
+        </S.ClubCardWrapper>
+    </>
+  );
+}
+
+export default ClubCard;
