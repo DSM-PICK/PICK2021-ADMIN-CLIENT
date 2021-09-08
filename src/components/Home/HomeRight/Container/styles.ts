f1e95a4bf1ea8860a99dffd97306ac8faeee82f7
oajh04 <cloudtubekr@naver.com>
2021-09-08 22:03:07 +0900

    ⚡️ :: (#10) HomeRight Container

---
+++ src/components/Home/HomeRight/Container/styles.ts
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled'
+import { color } from '../../../../style/color'
+
+export const ContainerWrapper = styled.div`
+    width: 500px;
+    margin: 0 auto;
+    margin-bottom: 8px;
+    margin-top: 40px;
+    display: grid;
+    text-align: center;
+    grid-template-columns: 95px 205px 105px 95px;
+    color: ${color.gray};
+`
