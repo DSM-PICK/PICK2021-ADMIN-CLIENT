8287aea363b9844083577c5295198b0f66ca39f1
oajh04 <cloudtubekr@naver.com>
2021-09-08 22:02:23 +0900

    ⚡️ :: (#10) min-width 추가

--- src/style/globalStyle.ts
+++ src/style/globalStyle.ts
@@ -3,6 +3,7 @@ import { css } from "@emotion/react";
 
 export const GlobalStyle = css`
     body {
+        min-width: 1160px;
         background-color: #FAFAFA;
         margin: 0;
         padding: 0;
