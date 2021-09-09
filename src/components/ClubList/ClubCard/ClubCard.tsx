8223a3c9cdda4f89581d1937920fb809bd749abb
oajh04 <cloudtubekr@naver.com>
2021-09-09 15:34:42 +0900

    ⚡️ :: (#13) ClubCard

--- src/components/ClubList/ClubCard/ClubCard.tsx
+++ src/components/ClubList/ClubCard/ClubCard.tsx
@@ -5,7 +5,12 @@ const ClubCard = () => {
   return (
     <>
         <S.ClubCardWrapper>
-            
+            <S.ClubName>SEMICOLON;</S.ClubName>
+            <S.ClubDes>
+                <S.ClubInfo>소프트웨어개발 2실</S.ClubInfo>
+                <S.Line/>
+                <S.ClubInfo>한준호</S.ClubInfo>
+            </S.ClubDes>
         </S.ClubCardWrapper>
     </>
   );
