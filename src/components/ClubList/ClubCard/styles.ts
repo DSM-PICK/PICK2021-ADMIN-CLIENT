482658e89146703d2f43bbce91d0d3cdaefbf06b
oajh04 <cloudtubekr@naver.com>
2021-09-09 15:34:52 +0900

    ⚡️ :: (#13) ClubList

--- src/components/ClubList/ClubCard/styles.ts
+++ src/components/ClubList/ClubCard/styles.ts
@@ -7,4 +7,36 @@ export const ClubCardWrapper = styled.div`
     background-color: ${color.white};
     margin: 0 auto;
     margin-bottom: 32px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    cursor: pointer;
+`
+
+export const ClubDes = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 24px;
+`
+
+export const Line = styled.div`
+    width: 1px;
+    height: 36px;
+    margin: 0 24px;
+    background-color: ${color.gray2};
+`
+
+export const ClubName = styled.div`
+    font-family: Noto Sans KR;
+    font-style: normal;
+    font-size: 40px;
+    text-align: center;
+`
+
+export const ClubInfo = styled.div`
+    font-family: Noto Sans KR;
+    font-style: normal;
+    font-size: 24px;
+    text-align: center;
 `
