5ee91342d37ded0a5c9fd464287e314edc4dbe3a
oajh04 <cloudtubekr@naver.com>
2021-09-10 18:44:07 +0900

    ⚡️ :: (#16) EditBar style

--- src/components/SideBar/EditBar/styles.ts
+++ src/components/SideBar/EditBar/styles.ts
@@ -1,3 +1,44 @@
 import styled from '@emotion/styled'
+import { color } from '../../../style/color'
 
-export const 
+export const EditBarIconBox = styled.div`
+    width: 80px;
+    height: 80px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    cursor: pointer;
+    img {
+        width: 30px;
+        height: 30px;
+        margin: 16px 24px;
+        margin-bottom: 6px;
+    }
+`
+
+export const EditBarIconBoxLast = styled.div`
+    position: absolute;
+    top: 100%;
+    transform: translateY(-120%);
+    cursor: pointer;
+    img {
+        width: 30px;
+        height: 30px;
+        margin: 16px 24px;
+        margin-bottom: 6px;
+    }
+    div{
+        color: ${color.red};
+        text-align: center;
+    }
+`
+
+export const EditBarText = styled.div`
+    font-family: Noto Sans KR;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 15px;
+    line-height: 14px;
+    text-align: center;
+    color: ${color.gray};
+`
