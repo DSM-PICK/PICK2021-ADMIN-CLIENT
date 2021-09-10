c707c07e04bd8f207640805ef2a26931e79e50c4
oajh04 <cloudtubekr@naver.com>
2021-09-10 18:43:44 +0900

    ⚡️ :: (#16) SideBar

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -2,18 +2,16 @@ import React from 'react';
 import * as S from './styles'
 import { FC } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
-import MainRouter from '../../router/MainRouter';
-
-interface Props {
-  match: RouteComponentProps
-}
+import MainBar from './MainBar/MainBar';
+import EditBar from './EditBar/EditBar';
 
 const SideBar = () => {
 
   return (
     <>
       <S.SideBarWrapper >
-        <MainRouter />
+        <MainBar />
+        {/* <EditBar /> */}
       </S.SideBarWrapper>
     </>
   );
