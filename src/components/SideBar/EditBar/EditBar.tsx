c01030aeaff279604e0f9bd5fdec8dba09e7ca2a
oajh04 <cloudtubekr@naver.com>
2021-09-10 18:44:22 +0900

    ⚡️ :: (#16) EditBar

--- src/components/SideBar/EditBar/EditBar.tsx
+++ src/components/SideBar/EditBar/EditBar.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Back, Delete, Edit } from '../../../assets';
+import * as S from './styles'
 
 const EditBar = () => {
   return (
     <>
-        <S
+        <S.EditBarIconBox>
+            <img src={Back} alt="뒤로가기"/>
+        </S.EditBarIconBox>
+        <S.EditBarIconBox>
+            <img src={Edit} alt="수정"/>
+            <S.EditBarText>편집</S.EditBarText>
+        </S.EditBarIconBox>
+        <S.EditBarIconBoxLast>
+            <img src={Delete} alt="삭제" />
+            <div>삭제</div>
+        </S.EditBarIconBoxLast>
     </>
   );
 }
