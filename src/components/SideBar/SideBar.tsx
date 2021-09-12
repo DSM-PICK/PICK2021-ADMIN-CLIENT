9682e7891a1a1747088b10e875b2b37f9e2789ae
oajh04 <cloudtubekr@naver.com>
2021-09-12 20:35:45 +0900

    ⚡️ :: (#20) SideBar

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import * as S from './styles'
 import { FC } from 'react';
-import { RouteComponentProps } from 'react-router-dom';
+import { RouteComponentProps, useLocation } from 'react-router-dom';
 import MainBar from './MainBar/MainBar';
 import EditBar from './EditBar/EditBar';
 
 const SideBar = () => {
+  const location = useLocation();
 
   return (
     <>
-      <S.SideBarWrapper >
-        <MainBar />
-        {/* <EditBar /> */}
-      </S.SideBarWrapper>
+      {
+        location.pathname !== '/login' ?
+        <S.SideBarWrapper >
+          {
+            location.pathname === '/clubinfo' ?
+            <EditBar />
+            : <MainBar url={location.pathname}/>
+          }
+        </S.SideBarWrapper>
+        : <div></div>
+      }
     </>
   );
 }
