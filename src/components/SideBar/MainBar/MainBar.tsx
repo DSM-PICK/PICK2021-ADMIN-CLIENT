a3985b1574de34153e3ceae08f0129bf95aa58d4
oajh04 <cloudtubekr@naver.com>
2021-09-12 20:36:04 +0900

    ⚡️ :: (#20) MainBar

--- src/components/SideBar/MainBar/MainBar.tsx
+++ src/components/SideBar/MainBar/MainBar.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
+import { FC } from 'react';
 import { Link } from 'react-router-dom';
 import { Club, ClubOrange, Home, HomeOrange, LogOut, Upload, UploadOrange } from '../../../assets';
 import * as S from './styles'
+interface Props{
+    url: string
+}
 
-const MainBar = () => {
+const MainBar:FC<Props> = ({url}) => {
+    console.log(url)
+
   return (
     <>
         <S.SideBarIconBox>
             <Link to='/'>
-                <img src={Home} alt="홈"/> 
-                {/* <img src={HomeOrange} alt="홈"/> */}
+                <img src={url === '/' ? HomeOrange : Home} alt="홈"/> 
                 <S.SideBarText>홈</S.SideBarText>
             </Link>
         </S.SideBarIconBox>
 
         <S.SideBarIconBox>
             <Link to="/clublist">
-                <img src={Club} alt="동아리"/>
+                <img src={url === '/clublist' ? ClubOrange : Club} alt="동아리"/>
                 {/* <img src={ClubOrange} alt="동아리"/> */}
                 <S.SideBarText>동아리</S.SideBarText>
             </Link>
@@ -24,7 +29,7 @@ const MainBar = () => {
 
         <S.SideBarIconBox>
             <Link to="/file-upload">
-                <img src={Upload} alt="업로드"/>
+                <img src={url === '/file-upload' ? UploadOrange : Upload} alt="업로드"/>
                 {/* <img src={UploadOrange} alt="업로드"/> */}
                 <S.SideBarText>데이터</S.SideBarText>
             </Link>
