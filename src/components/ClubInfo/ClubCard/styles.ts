c6e17df79289fc0dbc2f2cbee3174a8108b2c922
oajh04 <cloudtubekr@naver.com>
2021-09-13 22:54:29 +0900

    ⚡️ :: (#14) ClubCard style

---
+++ src/components/ClubInfo/ClubCard/styles.ts
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+import { color } from "../../../style/color";
+
+export const ClubCard = styled.div`
+    width: 280px;
+    height: 100px;
+    margin-bottom: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+    background-color: ${color.white};
+`
