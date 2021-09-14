e0043bcad9c30bd76d34d08a8294ce390a1445af
oajh04 <cloudtubekr@naver.com>
2021-09-14 22:40:49 +0900

    ⚡️ :: (#25) Modal style

---
+++ src/components/Container/Modal/styles.ts
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+import { color } from '../../../style/color';
+
+export const BasicModalWrapper = styled.div`
+    z-index: 2;
+    margin: 0 auto;
+    width: 460px;
+    height: 180px;
+    position: relative;
+    top: 100px;
+    transform: translateY(-25%);
+    background-color: ${color.white};
+`
