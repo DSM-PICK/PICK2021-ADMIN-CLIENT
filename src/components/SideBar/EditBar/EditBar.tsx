77970bf10e2e263150cca63601f7654ff5535cc2
oajh04 <cloudtubekr@naver.com>
2021-10-29 00:07:15 +0900

    ⚡️ :: (#29) edit 바에 뒤로가기 추가

--- src/components/SideBar/EditBar/EditBar.tsx
+++ src/components/SideBar/EditBar/EditBar.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { Back, Delete, Edit } from '../../../assets';
 import * as S from './styles'
 
 const EditBar = () => {
+  const history = useHistory()
+  const onBack = () => {
+    history.goBack()
+  }
+
   return (
     <>
         <S.EditBarIconBox>
-            <img src={Back} alt="뒤로가기"/>
+            <img src={Back} alt="뒤로가기" onClick={onBack}/>
         </S.EditBarIconBox>
         <S.EditBarIconBox>
             <img src={Edit} alt="수정"/>
