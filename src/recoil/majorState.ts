7a4a86d2a20fb0e4db343ae6b1c64a5b7256b20e
oajh04 <cloudtubekr@naver.com>
2021-11-08 23:33:07 +0900

    ⚡️ :: (#38) majorState recoil

---
+++ src/recoil/majorState.ts
@@ -0,0 +1,28 @@
+import { atom, selector, selectorFamily } from 'recoil'
+import majorApi from '../libs/api/major/majorApi'
+
+interface majorState {
+    major_id: number,
+    major_name: string,
+    location_name: string,
+    head_name: string
+}
+
+export const majorListState = atom({
+    key: 'majorListState',
+    default: []
+})
+
+export const majorListSelector = selector({
+    key: 'majorListSeletor',
+    get: async ({get}) => {
+        let list = await majorApi.getMajorList()
+        .then((res) => {
+            return res.data;
+        })
+        .catch((res) => {
+            return res.data
+        })
+        return list
+    },
+})
