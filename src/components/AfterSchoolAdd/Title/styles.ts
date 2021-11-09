dc23780252001611bdb598fb9778452accaa78f5
oajh04 <cloudtubekr@naver.com>
2021-11-09 23:42:21 +0900

    ⚡️ :: 방과후 추가하기 title 스타일

---
+++ src/components/AfterSchoolAdd/Title/styles.ts
@@ -0,0 +1,35 @@
+import styled from '@emotion/styled';
+
+export const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+`
+
+export const Title = styled.div`
+    width: 300px;
+    border-bottom: 1px solid black;
+    input {
+        background: none;
+        width: 300px;
+        font-size: 24px;
+        outline: none;
+        border: none;
+        text-align: center;
+    }
+`
+
+export const Place = styled.div`
+    width: 150px;
+    margin-top: 5px;
+    border-bottom: 1px solid black;
+    input {
+        background: none;
+        width: 100px;
+        font-size: 22px;
+        outline: none;
+        border: none;
+        text-align: center;
+    }
+`
