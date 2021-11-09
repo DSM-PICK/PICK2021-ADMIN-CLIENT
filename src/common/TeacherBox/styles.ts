b6cc5c506f04f1fc36e89d209284e525203eb2db
oajh04 <cloudtubekr@naver.com>
2021-11-09 23:49:44 +0900

    ⚡️ :: 선생님 리스트 스타일

---
+++ src/common/TeacherBox/styles.ts
@@ -0,0 +1,17 @@
+import styled from "@emotion/styled";
+
+export const TeacherBoxWrapper = styled.select`
+    width: 150px;
+    height: 20px;
+    background: none;
+    outline: none;
+    border: none;
+    text-align: center;
+    appearance: none;
+`
+
+export const Teacher = styled.option`
+    font-size: 14px;
+    padding: 5px;
+    cursor: pointer;
+`
