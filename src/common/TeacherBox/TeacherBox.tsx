4b6f38268fdc6acb75ab598a66199feb9501af0a
oajh04 <cloudtubekr@naver.com>
2021-11-09 23:49:31 +0900

    ⚡️ :: 선생님 리스트 select

---
+++ src/common/TeacherBox/TeacherBox.tsx
@@ -0,0 +1,57 @@
+import React, { ReactComponentElement, ReactEventHandler } from 'react';
+import { useState } from 'react';
+import { useRecoilValue } from 'recoil';
+import { Teacher } from '../../components/AccountManagement/TeacherList/styles';
+import { teacherDataState } from '../../recoil/userDataState';
+import * as S from './styles'
+
+const teacherList = [
+    {
+        teacher_id: 'dsm00',
+        name: '아무개'
+    },
+    {
+        teacher_id: 'dsm01',
+        name: '아기개'
+    },
+    {
+        teacher_id: 'dsm02',
+        name: '아수개'
+    },
+    {
+        teacher_id: 'dsm03',
+        name: '아자개'
+    },
+    {
+        teacher_id: 'dsm04',
+        name: '아재개'
+    },
+    {
+        teacher_id: 'dsm05',
+        name: '아푸개'
+    }
+];
+
+const TeacherBox = () => {
+    // const teacherList = useRecoilValue(teacherDataState);
+    const [ option, setOption, ] = useState('choice')
+
+    const handleSelect = (e: any) => {
+        setOption(e.target.value);
+    };
+
+    return (
+        <>
+            <S.TeacherBoxWrapper value={option} onChange={handleSelect}>
+                <S.Teacher key='choice' value='choice'>선택해주세요</S.Teacher>
+                {
+                    teacherList.map((i: any, index: any) => {
+                        return <S.Teacher key={i.teacher_id} value={i.teacher_id}>{i.name}</S.Teacher>
+                    })
+                }
+            </S.TeacherBoxWrapper>
+        </>
+    );
+}
+
+export default TeacherBox;
