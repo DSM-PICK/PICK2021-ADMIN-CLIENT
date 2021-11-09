7e86ccfe6221c559fb681a9310088065ccef2e74
oajh04 <cloudtubekr@naver.com>
2021-11-09 23:40:03 +0900

    ⚡️ :: 선생님 계정관리 리스트

---
+++ src/components/AccountManagement/TeacherList/TeacherList.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { teacherDataState, teacherState } from '../../../recoil/userDataState';
+import * as S from './styles'
+
+const TeacherList = () => {
+    const [ teaceherId , setTeacherId] = useRecoilState(teacherState);
+    const teacherlist = useRecoilValue(teacherDataState);
+    console.log(teacherlist)
+
+    const onTeacherInfo = (teacher_id: string) => {
+        setTeacherId(teacher_id)
+    }
+
+    return (
+        <>
+            <S.TeacherListWrapper>
+                <span>선생님 목록</span>
+                <S.TeacherListBox>
+                    {
+                        Array(19).fill(-1).map((i,index) => {
+                            return (
+                                <S.Teacher key={i.id} isActive={index === 2} onClick={() => onTeacherInfo(i.teacher_id)}>i.name</S.Teacher>
+                            )
+                        })
+                    }
+                    {/* {
+                        teacherlist.map((i: any) =>{
+                            return (
+                                <S.Teacher key={i.id} isActive={i.teacher_id === teaceherId} onClick={() => onTeacherInfo(i.teacher_id)}>{i.name}</S.Teacher>
+                            )
+                        })
+                    } */}
+                </S.TeacherListBox>
+            </S.TeacherListWrapper>
+        </>
+    );
+}
+
+export default TeacherList;
+
+function index(i: any, index: any): string | number | boolean | {} | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactNodeArray | React.ReactPortal | null | undefined {
+    throw new Error('Function not implemented.');
+}
