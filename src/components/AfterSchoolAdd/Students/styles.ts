f7891a7807b8b11371522870d21dc6a2eb3d1fb2
oajh04 <cloudtubekr@naver.com>
2021-11-10 23:05:47 +0900

    ⚡️ :: 방과후 추가하기 학생 리스트 스타일

---
+++ src/components/AfterSchoolAdd/Students/styles.ts
@@ -0,0 +1,17 @@
+import styled from "@emotion/styled";
+
+export const StudentsWrapper = styled.div`
+    height: 480px;
+    overflow: auto;
+    display: grid;
+    justify-content: center;
+    margin-top: 20px;
+    grid-template-columns: repeat(6, 250px);
+    grid-auto-rows: 120px;
+    @media screen and (max-width: 1600px) { 
+        grid-template-columns: repeat(5, 250px);
+    }
+    @media screen and (max-width: 1340px) { 
+        grid-template-columns: repeat(3, 250px);
+    }
+`
