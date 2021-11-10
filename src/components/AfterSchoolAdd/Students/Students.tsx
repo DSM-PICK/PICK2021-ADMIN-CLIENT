7d81f5035b32a2626e4d5a1a367ad9310ede2cb3
oajh04 <cloudtubekr@naver.com>
2021-11-10 23:05:37 +0900

    ⚡️ :: 방과후 추가하기 학생 리스트

--- src/components/AfterSchoolAdd/Students/Students.tsx
+++ src/components/AfterSchoolAdd/Students/Students.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import AddCard from './AddCard/AddCard';
+import Card from './Card/Card';
+import * as S from './styles'
 
 const Students = () => {
   return (
     <>
-        
+      <S.StudentsWrapper>
+        {
+          Array(0).fill(-1).map(() => <Card />)
+        }
+        <AddCard/>
+      </S.StudentsWrapper>
     </>
   );
 }
