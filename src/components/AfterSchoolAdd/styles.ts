aca8812acce052aea71ad267a73d84a60e4b886c
oajh04 <cloudtubekr@naver.com>
2021-11-10 23:04:00 +0900

    :coffin: :: 방과후 추가 style 제거

--- src/components/AfterSchoolAdd/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled';
-
-export const Wrapper = styled.div`
-    width: 1400px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-`
