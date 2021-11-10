294c7adfaba1af338f8a60e596e9d40d7254952b
oajh04 <cloudtubekr@naver.com>
2021-11-10 23:03:42 +0900

    ⚡️ :: style 제거

--- src/components/AfterSchoolAdd/AfterSchoolAdd.tsx
+++ src/components/AfterSchoolAdd/AfterSchoolAdd.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import ASInfo from './ASInfo/ASInfo';
 import Students from './Students/Students';
 import Title from './Title/Title';
-import * as S from './styles'
 
 const AfterSchoolAdd = () => {
+  
+
   return (
     <>
-      <S.Wrapper>
         <Title />
         <ASInfo />
+        <hr />
         <Students />
-      </S.Wrapper>
     </>
   );
 }
