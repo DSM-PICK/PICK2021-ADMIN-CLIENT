e855d9713dcf610b30c42516808260a4db5f68c9
oajh04 <cloudtubekr@naver.com>
2021-11-10 23:07:41 +0900

    ⚡️ :: as sub bar 스타일 수정

--- src/components/AfterSchoolInfo/AfterSchoolSubBar/AfterSchoolSubBar.tsx
+++ src/components/AfterSchoolInfo/AfterSchoolSubBar/AfterSchoolSubBar.tsx
@@ -6,14 +6,18 @@ const AfterSchoolSubBar = () => {
   return (
     <>
         <S.AfterSchoolSubBarWrapper>
-            <div>
+            <S.ASInfoLeft>
                 <div>총원 : 18명</div>
                 <div>담당 : 정창용</div>
-            </div>
-            <S.StudentSearch>
-                <input placeholder="학번 또는 이름"/>
-                <img src={Search} alt='검색'/>
-            </S.StudentSearch>
+            </S.ASInfoLeft>
+            <S.ASInfoRight>
+              <S.StudentSearch>
+                  <input placeholder="학번 또는 이름"/>
+                  <button><img src={Search} alt='검색'/></button>
+              </S.StudentSearch>
+              <S.ASButton>수정</S.ASButton>
+              <S.ASButton>삭제</S.ASButton>
+            </S.ASInfoRight>
         </S.AfterSchoolSubBarWrapper>
     </>
   );
