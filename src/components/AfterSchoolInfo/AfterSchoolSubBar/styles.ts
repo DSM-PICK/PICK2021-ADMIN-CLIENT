f8d86cce03488de8199ca248aa78abe309a529e9
oajh04 <cloudtubekr@naver.com>
2021-11-10 23:09:06 +0900

    ⚡️ :: as sub bar 스타일 수정

--- src/components/AfterSchoolInfo/AfterSchoolSubBar/styles.ts
+++ src/components/AfterSchoolInfo/AfterSchoolSubBar/styles.ts
@@ -2,8 +2,20 @@ import styled from '@emotion/styled'
 import { color } from '../../../style/color'
 
 export const AfterSchoolSubBarWrapper = styled.div`
-    width: 1200px;
+    width:calc(100% - 50px);
     margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    @media screen and (max-width: 1600px) { 
+        width: 1000px;
+    }
+    @media screen and (max-width: 1340px) { 
+        width: 700px;
+    }
+`
+
+export const ASInfoLeft = styled.div`
+    width: 250px;
     display: flex;
     justify-content: space-between;
     div{
@@ -14,16 +26,34 @@ export const AfterSchoolSubBarWrapper = styled.div`
         display: flex;
         align-items: center;
     }
-    @media screen and (max-width: 1600px) { 
-        width: 1000px;
+`
+
+export const ASInfoRight = styled.div`
+    width: 430px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+
+export const ASButton = styled.button`
+    width: 100px;
+    height: 30px;
+    font-size: 20px;
+    border: none;
+    border-radius: 20px;
+    outline: none;
+    color: white;
+    cursor: pointer;
+    :nth-of-type(1) {
+        background-color: ${color.orange};
     }
-    @media screen and (max-width: 1340px) { 
-        width: 700px;
+    :nth-of-type(2) {
+        background-color: ${color.red};
     }
 `
 
-export const StudentSearch = styled.div`
-    width: 200px;
+export const StudentSearch = styled.form`
+    width: 210px;
     height: 45px;
     display: flex;
     align-items: center;
@@ -32,15 +62,20 @@ export const StudentSearch = styled.div`
     input{
         width: 140px;
         height: 25px;
-        padding: 5px;
+        padding: 10px;
         border: none;
         font-size: 20px;
         outline: none;
     }
-    img{
-        width: 30px;
-        height: 30px;
-        cursor: pointer;
-        padding-right: 5px;
+    button{
+        border: none;
+        outline: none;
+        background: none;
+        img{
+            width: 30px;
+            height: 30px;
+            cursor: pointer;
+            padding-right: 5px;
+        }
     }
 `
