71dd07a3350f97f6c77f0a7b8ac24c6a416d6f88
oajh04 <cloudtubekr@naver.com>
2021-11-10 23:05:16 +0900

    ⚡️ :: 방과후 추가하기 학생 카드

---
+++ src/components/AfterSchoolAdd/Students/Card/Card.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import * as S from './styles'
+
+const Card = () => {
+  return (
+    <>
+        <S.CardWrpper>
+          2121 한준호
+        </S.CardWrpper>
+    </>
+  );
+}
+
+export default Card;
