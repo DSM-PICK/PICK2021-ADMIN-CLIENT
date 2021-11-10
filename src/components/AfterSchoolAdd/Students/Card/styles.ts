0ea6feb079a87fe0e2334634e181da6154b80de7
oajh04 <cloudtubekr@naver.com>
2021-11-10 23:05:25 +0900

    ⚡️ :: 방과후 추가하기 학생 카드 스타일

---
+++ src/components/AfterSchoolAdd/Students/Card/styles.ts
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled/macro";
+import { color } from "../../../../style/color";
+
+export const CardWrpper = styled.div`
+    width: 200px;
+    height: 100px;
+    margin: 0 auto;
+    margin-bottom: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: ${color.white};
+    font-size: 24px;
+`
