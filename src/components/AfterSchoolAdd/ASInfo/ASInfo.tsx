013d3200d60d25c64474bd4a13458807205d7a92
oajh04 <cloudtubekr@naver.com>
2021-11-10 23:04:17 +0900

    ⚡️ :: 방과후 추가하기 검색 삭제 및 생성버튼 추가

--- src/components/AfterSchoolAdd/ASInfo/ASInfo.tsx
+++ src/components/AfterSchoolAdd/ASInfo/ASInfo.tsx
@@ -10,10 +10,7 @@ const ASInfo = () => {
             <S.PeopleCount>총원 0명</S.PeopleCount>
             <input placeholder="담당선생님"/>
           </S.Left>
-          <S.Right>
-            <input placeholder="학번 또는 이름"/>
-            <img src={Search} alt="검색"/>
-          </S.Right>
+          <S.ASAddButton>생성하기</S.ASAddButton>
         </S.Wrapper>
     </>
   );
