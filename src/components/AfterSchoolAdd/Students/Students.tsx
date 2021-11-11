72061d44f342057e1fb660d23031923a5e5cc539
oajh04 <cloudtubekr@naver.com>
2021-11-11 20:30:25 +0900

    ⚡️ :: AS Students

--- src/components/AfterSchoolAdd/Students/Students.tsx
+++ src/components/AfterSchoolAdd/Students/Students.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import { useRecoilValue } from 'recoil';
+import { studentAddListState } from '../../../recoil/userDataState';
 import AddCard from './AddCard/AddCard';
 import Card from './Card/Card';
 import * as S from './styles'
 
 const Students = () => {
+  const studentAddList = useRecoilValue(studentAddListState)
   return (
     <>
       <S.StudentsWrapper>
         {
-          Array(0).fill(-1).map(() => <Card />)
+          studentAddList.map((i: any) => {
+            return (
+              <Card {...i} key={i.student_id}/>
+            )
+          })
         }
         <AddCard/>
       </S.StudentsWrapper>
