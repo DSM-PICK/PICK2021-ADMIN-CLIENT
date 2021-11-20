dfffca0840b42b370f6ed6176fec19c1077a0eef
oajh04 <cloudtubekr@naver.com>
2021-11-20 22:24:52 +0900

    ⚡️ :: AS style z-index 수정

--- src/components/AfterSchoolInfo/AfterSchoolCard/PersonSet/styles.ts
+++ src/components/AfterSchoolInfo/AfterSchoolCard/PersonSet/styles.ts
@@ -9,14 +9,14 @@ export const PersonSetWrapper = styled.div`
     justify-content: space-around;
     text-align: center;
     img{
-        z-index: 2;
+        z-index: 99;
         width: 28px;
         height: 28px;
     }
 `
 
 export const Card = styled.div<Props>`
-    z-index: 2;
+    z-index: 99;
     cursor: pointer;
     color: ${(props) => props.color};
 `
