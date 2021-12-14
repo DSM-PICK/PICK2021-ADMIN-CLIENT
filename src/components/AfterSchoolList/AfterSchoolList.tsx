a719a437869b63037b60af7bd84fbdf4379856f8
oajh04 <cloudtubekr@naver.com>
2021-12-14 22:32:32 +0900

    ⚡️ :: 동아리 리스트 및 방과후 리스트 api 연동

--- src/components/AfterSchoolList/AfterSchoolList.tsx
+++ src/components/AfterSchoolList/AfterSchoolList.tsx
@@ -1,4 +1,4 @@
-import { Link, RouteComponentProps, useHistory } from 'react-router-dom';
+import { RouteComponentProps, useHistory } from 'react-router-dom';
 import AfterSchoolBar from './AfterSchoolBar/AfterSchoolBar';
 import AfterSchoolCard from './Card/Card';
 import { useEffect } from 'react';
@@ -7,22 +7,55 @@ import * as S from './styles'
 import { useRecoilState } from 'recoil';
 import { asMoveState } from '../../recoil/asMoveState';
 import AfterSchoolMove from '../Container/Modal/AfterSchoolMove/AfterSchoolMove';
+import { useLayoutEffect } from 'react';
+import majorApi from '../../libs/api/major/majorApi';
+import activityApi from '../../libs/api/activity/activityApi';
+
+interface IMajor {
+  major_id: number,
+  major_name: string,
+  location_name: string,
+  head_name: string
+}
+
+interface IActivity {
+  after_school_id: number,
+  name: string,
+  teacher_id: string,
+  location_name: string,
+}
 
 const AfterSchoolList = ({ location, match } : RouteComponentProps<{type: string}>) => {
   const [ path, setPath ] = useState<string>('');
-  const [ data, setData ] = useRecoilState(asMoveState)
+  const [ moveData, setMoveData ] = useRecoilState(asMoveState)
+  const [ data, setData ] = useState<any[]>([])
   const type = match.params.type;
 
   const history = useHistory()
+
+  useLayoutEffect(()=> {
+    console.log(type)
+    if(type === 'major'){
+      majorApi.getMajorList()
+      .then((res) => {
+        setData(res.data)
+      })
+    } else {
+      activityApi.getActivityList()
+      .then((res) => {
+        setData(res.data)
+      })
+    }
+  },[type])
 
   useEffect(() => {
     setPath(location.pathname.split('-')[0])
   }, [location])
 
   const onGoAfterSchool = (index: number) => {
-    !data.isChange ?
+    !moveData.isChange ?
     history.push(`/as-info/${type}/${index}`)
-    : setData({...data, afterAsId: index, afterAsName: 'pick'})
+    : setMoveData({...moveData, afterAsId: index, afterAsName: 'pick'})
   }  
 
   return (
@@ -31,10 +64,14 @@ const AfterSchoolList = ({ location, match } : RouteComponentProps<{type: string
             <AfterSchoolBar />
             <S.AfterSchoolListBox>
                 {
-                    Array(7).fill(-1).map((_, index) => {
+                    data.map((i, index) => {
                     return (
                       <div onClick={() => onGoAfterSchool(index)} key={index}>
-                        <AfterSchoolCard/>
+                        {
+                          type === 'major' ?
+                          <AfterSchoolCard id={i.major_id} location={i.location_name} asName={i.major_name} userName={i.head_name}/>
+                          : <AfterSchoolCard id={i.after_school_id} location={i.location_name} asName={i.name} userName={i.teacher_id}/>
+                        }
                       </div>
                     )
                     
@@ -42,7 +79,7 @@ const AfterSchoolList = ({ location, match } : RouteComponentProps<{type: string
                 }
             </S.AfterSchoolListBox>
             {
-              data.isChange===true && data.type === 'major' &&
+              moveData.isChange === true && moveData.type === 'major' &&
               <AfterSchoolMove />
             }
         </S.AfterSchoolListWrapper>
