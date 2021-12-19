a6d0999279ccbb818b170b06e1a9829dfd08ff99
oajh04 <cloudtubekr@naver.com>
2021-12-19 22:14:57 +0900

    ⚡️ :: asInfo SubBar

--- src/components/AfterSchoolInfo/AfterSchoolSubBar/AfterSchoolSubBar.tsx
+++ src/components/AfterSchoolInfo/AfterSchoolSubBar/AfterSchoolSubBar.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
+import { useRecoilValue } from 'recoil';
 import { Search } from '../../../assets';
+import activityApi from '../../../libs/api/activity/activityApi';
+import majorApi from '../../../libs/api/major/majorApi';
+import { asInfoState } from '../../../recoil/asInfoState';
 import * as S from './styles'
 
 const AfterSchoolSubBar = () => {
+  const asInfoData = useRecoilValue(asInfoState)
+
+  const onASDelete = () => {
+    if(asInfoData.type === 'major'){
+      majorApi.deleteMajor(asInfoData.id)
+    } else {
+      activityApi.deleteActivity(asInfoData.id)
+    }
+  }
+
   return (
     <>
         <S.AfterSchoolSubBarWrapper>
             <S.ASInfoLeft>
-                <div>총원 : 18명</div>
-                <div>담당 : 정창용</div>
+                <div>총원 : {asInfoData.count}명</div>
+                <div>담당 : {asInfoData.teacher_name}</div>
             </S.ASInfoLeft>
             <S.ASInfoRight>
               <S.StudentSearch>
@@ -16,7 +30,7 @@ const AfterSchoolSubBar = () => {
                   <button><img src={Search} alt='검색'/></button>
               </S.StudentSearch>
               <S.ASButton>수정</S.ASButton>
-              <S.ASButton>삭제</S.ASButton>
+              <S.ASButton onClick={onASDelete}>삭제</S.ASButton>
             </S.ASInfoRight>
         </S.AfterSchoolSubBarWrapper>
     </>
