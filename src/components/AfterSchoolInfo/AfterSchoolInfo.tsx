8652cc3db8985aeb609ca82b721c41df9703268c
oajh04 <cloudtubekr@naver.com>
2021-12-19 22:14:25 +0900

    ⚡️ :: asInfo

--- src/components/AfterSchoolInfo/AfterSchoolInfo.tsx
+++ src/components/AfterSchoolInfo/AfterSchoolInfo.tsx
@@ -1,16 +1,35 @@
 import React, { FC } from 'react';
+import { useState } from 'react';
+import { useLayoutEffect } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
-import { BasicModal } from '../Container';
+import { useRecoilState } from 'recoil';
+import majorApi from '../../libs/api/major/majorApi';
+import { asInfoState } from '../../recoil/asInfoState';
 import AfterSchoolCard from './AfterSchoolCard/AfterSchoolCard';
 import AfterSchoolName from './AfterSchoolName/AfterSchoolName';
 import AfterSchoolSubBar from './AfterSchoolSubBar/AfterSchoolSubBar';
 import * as S from './styles'
 
 interface Props {
-  id: string
+  type: string;
+  id: any
 }
 
 const AfterSchoolInfo:FC<RouteComponentProps<Props>> = ({match}) => {
+  const [ asInfoData, setASInfoData ] = useRecoilState(asInfoState)
+  const type = match.params.type;
+  const id = match.params.id;
+
+  useLayoutEffect(() => {
+    if(type === 'major'){
+      majorApi.getMajorInfo(id)
+      .then((res) => {
+        setASInfoData({...res.data, type: type, id: id})
+      })
+    }
+  })
+
+
   return (
     <>
         <AfterSchoolName />
@@ -18,9 +37,9 @@ const AfterSchoolInfo:FC<RouteComponentProps<Props>> = ({match}) => {
         <hr />
         <S.CardWrapper>
           {
-            Array(30).fill(-1).map((i,index) => {
+            asInfoData.members.map((i: any,index: number) => {
               return (
-                <AfterSchoolCard key={index} index={index}/>
+                <AfterSchoolCard key={index} index={index} id={i.student_id} name={i.student_name} gcn={i.gcn}/>
               )
             })
           }
