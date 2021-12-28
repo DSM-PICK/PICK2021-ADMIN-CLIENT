163c688ad6ce6cc9c376ef799f0aa4db78e819e6
oajh04 <cloudtubekr@naver.com>
2021-12-28 23:06:35 +0900

    ⚡️ :: (#51) suspense loading 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Global } from '@emotion/react';
 import { Suspense } from 'react';
 import { RecoilRoot } from 'recoil';
+import Loading from './common/Loading/Loading';
 import RootRouter from './router/RootRouter';
 import { GlobalStyle } from './style/globalStyle';
 
@@ -9,7 +10,7 @@ function App() {
   return (
     <>
       <RecoilRoot>
-        <Suspense fallback={<div>...loading</div>}>
+        <Suspense fallback={<Loading/>}>
           <Global styles={GlobalStyle} />
           <RootRouter />
         </Suspense>
