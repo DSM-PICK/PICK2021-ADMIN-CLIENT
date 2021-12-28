4135af675fc4efbaf596581aa89969d86ca3cbd5
oajh04 <cloudtubekr@naver.com>
2021-12-28 23:14:42 +0900

    ⚡️ :: (#53) 학생 박스 수정

--- src/common/StudentBox/StudentBox.tsx
+++ src/common/StudentBox/StudentBox.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import { useRecoilState, useSetRecoilState } from 'recoil';
-import { studentAddListState, studentState } from '../../recoil/userDataState';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { FC } from 'react';
+import { useRecoilValue } from 'recoil';
+import teacherApi from '../../libs/api/teacher/teacherApi';
+import { studentAddListState } from '../../recoil/asAddState';
 import * as S from './styles'
 
 const state = [
     {
         student_id: 1,
-        gcn: '0000',
+        gcn: '0001',
         name: '한준호'
     },
     {
@@ -34,32 +38,53 @@ const state = [
         gcn: '0006',
         name: '이준호'
     },
+    {
+        student_id: 7,
+        gcn: '0007',
+        name: '심준호'
+    },
+    {
+        student_id: 8,
+        gcn: '0008',
+        name: '위준호'
+    },
+    {
+        student_id: 9,
+        gcn: '0009',
+        name: '노준호'
+    },
 ]
 
-const StudentBox = () => {
-    const [studentAddList,setStudentAddList] = useRecoilState(studentAddListState)
-    const setStudent = useSetRecoilState(studentState)
+interface Props {
+    student: string,
+    setStudentTemp: (id: number, gcn: string, name: string) =>  void,
+}
 
-    console.log(studentAddList)
-    const onStudentAdd = (id: number, gcn: string, name: string) => {
-        setStudent('')
-        setStudentAddList([
-            ...studentAddList,
-            {
-                student_id: id,
-                gcn: gcn,
-                name: name
-            }
-        ])
+const StudentBox: FC<Props> = ({student, setStudentTemp}) => {
+    const [ studentData, setStudentData ]  = useState([])
+    const [ filterData, setFilterData ] = useState<any[]>(state)
+    const studentAddList = useRecoilValue(studentAddListState);
+
+    useEffect(() => {
+        teacherApi.getStudentSearch(student)
+        .then((res) => {
+            setStudentData(res.data)
+        })
+        onStudentDatafilter()
+    }, [student])
+
+    const onStudentDatafilter = () => {
+        const filter = state.filter((data) => { return studentAddList.findIndex(i => i.student_id === data.student_id) < 0 })
+        setFilterData(filter)
     }
 
     return (
         <>
             <S.StudentBoxWrapper>
                 {
-                    state.map((i: any, idx: any) => {
+                    filterData.map((i: any, idx: any) => {
                         return (
-                            <S.StudentName key={i.student_id} onClick={() => onStudentAdd(i.student_id, i.gcn, i.name)}>
+                            <S.StudentName key={i.student_id} onClick={() => setStudentTemp(i.student_id, i.gcn, i.name)}>
                                 {i.gcn} {i.name}
                             </S.StudentName>
                         )
