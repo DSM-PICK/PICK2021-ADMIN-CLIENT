c94ce5b06467288605236361583b60e491e3d6b3
oajh04 <cloudtubekr@naver.com>
2022-07-04 18:42:50 +0900

    ⚡️ :: 방과후 추가하기

--- src/components/AfterSchoolAdd/Title/Title.tsx
+++ src/components/AfterSchoolAdd/Title/Title.tsx
@@ -1,43 +1,45 @@
-import React from 'react';
-import { useState } from 'react';
-import { useRecoilState, useRecoilValue } from 'recoil';
-import LocationBox from '../../../common/LocationBox/LocationBox';
-import { asAddTypeState, asInitState } from '../../../recoil/asAddState';
+import React from 'react'
+import { useRecoilState, useRecoilValue } from 'recoil'
+import LocationBox from '../../common/LocationBox/LocationBox'
+import { asAddTypeState, asInitState } from '../../../recoil/asAddState'
 import * as S from './styles'
 
 const Title = () => {
-  const [ asInitData, setAsInitData ] = useRecoilState(asInitState);
+  const [asInitData, setAsInitData] = useRecoilState(asInitState)
   const type = useRecoilValue(asAddTypeState)
 
   const onTitle = (e: any) => {
     setAsInitData({
       ...asInitData,
-      name: e.target.value
+      name: e.target.value,
     })
   }
 
   const onLocationId = (locationId: any) => {
     setAsInitData({
       ...asInitData,
-      location_id: locationId
+      location_id: locationId,
     })
   }
 
   return (
     <>
-        <S.Wrapper>
-            <S.Title>
-                <input placeholder={type === 'major' ? "동아리명" : '방과후명'} onChange={onTitle}/>
-            </S.Title>
+      <S.Wrapper>
+        <S.Title>
+          <input
+            placeholder={type === 'major' ? '동아리명' : '방과후명'}
+            onChange={onTitle}
+          />
+        </S.Title>
 
-          <div>
-            <S.Place>
-                {/* <LocationBox setLocationId={onLocationId}/> */}
-            </S.Place>
-          </div>
-        </S.Wrapper>
+        <div>
+          <S.Place>
+            <LocationBox setLocationId={onLocationId} />
+          </S.Place>
+        </div>
+      </S.Wrapper>
     </>
-  );
+  )
 }
 
-export default Title;
+export default Title
