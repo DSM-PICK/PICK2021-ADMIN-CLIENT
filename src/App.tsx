f2a61a1c103c10615099427c5b6da8488a06c0fb
oajh04 <cloudtubekr@naver.com>
2022-07-04 18:42:16 +0900

    ⚙️ :: 폴더구조 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
-import { Global } from '@emotion/react';
-import { Suspense } from 'react';
-import { RecoilRoot } from 'recoil';
-import Loading from './common/Loading/Loading';
-import RootRouter from './router/RootRouter';
-import { GlobalStyle } from './style/globalStyle';
+import { Global } from '@emotion/react'
+import { Suspense } from 'react'
+import { RecoilRoot } from 'recoil'
+import Loading from './components/common/Loading/Loading'
+import RootRouter from './router/RootRouter'
+import { GlobalStyle } from './style/globalStyle'
 
 function App() {
-
   return (
     <>
       <RecoilRoot>
-        <Suspense fallback={<Loading/>}>
+        <Suspense fallback={<Loading />}>
           <Global styles={GlobalStyle} />
           <RootRouter />
         </Suspense>
       </RecoilRoot>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
