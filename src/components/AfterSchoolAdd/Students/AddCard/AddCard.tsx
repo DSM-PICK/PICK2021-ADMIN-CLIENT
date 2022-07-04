cd8eb4fa0cb708f01d7bc2746a3b61c12c1b2719
oajh04 <cloudtubekr@naver.com>
2022-07-04 18:42:39 +0900

    ⚡️ :: 방과후 추가하기

--- src/components/AfterSchoolAdd/Students/AddCard/AddCard.tsx
+++ src/components/AfterSchoolAdd/Students/AddCard/AddCard.tsx
@@ -1,37 +1,42 @@
-import React, { useState } from 'react';
-import { useRecoilState } from 'recoil';
-import StudentBox from '../../../../common/StudentBox/StudentBox';
-import { studentAddListState } from '../../../../recoil/asAddState';
+import React, { useState } from 'react'
+import { useRecoilState } from 'recoil'
+import StudentBox from '../../../common/StudentBox/StudentBox'
+import { studentAddListState } from '../../../../recoil/asAddState'
 import * as S from './styles'
 
 const AddCard = () => {
   const [student, setStudent] = useState('')
-  const [studentAddList, setStudentAddList] = useRecoilState(studentAddListState)
+  const [studentAddList, setStudentAddList] =
+    useRecoilState(studentAddListState)
 
   const onStudentAdd = (id: number, gcn: string, name: string) => {
     setStudent('')
     setStudentAddList([
-        ...studentAddList,
-        {
-            student_id: id,
-            gcn: gcn,
-            name: name
-        }
+      ...studentAddList,
+      {
+        student_id: id,
+        gcn: gcn,
+        name: name,
+      },
     ])
-}
+  }
 
   return (
     <>
       <S.CardWrpper>
-          <input placeholder="학생 이름" onChange={(e) => setStudent(e.target.value)} value={student}/>
-          <div style={{position: 'relative'}}>
-            {
-              student.length !== 0 && <StudentBox student={student} setStudentTemp={onStudentAdd}/>
-            }
-          </div>
+        <input
+          placeholder="학생 이름"
+          onChange={(e) => setStudent(e.target.value)}
+          value={student}
+        />
+        <div style={{ position: 'relative' }}>
+          {student.length !== 0 && (
+            <StudentBox student={student} setStudentTemp={onStudentAdd} />
+          )}
+        </div>
       </S.CardWrpper>
     </>
-  );
+  )
 }
 
-export default AddCard;
+export default AddCard
