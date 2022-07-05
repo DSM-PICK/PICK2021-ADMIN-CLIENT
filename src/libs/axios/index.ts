fbd77b186569c8ac3eb0481165a90e4703b31f5c
oajh04 <cloudtubekr@naver.com>
2022-07-05 20:21:52 +0900

    ⚡️ :: axios

--- src/libs/axios/index.ts
+++ src/libs/axios/index.ts
@@ -1,25 +1,25 @@
-import axios, { AxiosError } from 'axios';
-import teacherApi from '../api/teacher/teacherApi';
+import axios, { AxiosError } from 'axios'
 
 const instance = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
   timeout: 10000,
-});
+})
 instance.interceptors.request.use(
   function (config) {
-    return config;
+    return config
   },
   function (error: AxiosError) {
-    return Promise.reject(error);
-  },
-);
+    return Promise.reject(error)
+  }
+)
 instance.interceptors.response.use(
   function (response) {
-    return response;
+    return response
   },
-  async error => {
-    const { config, response } = error;
-    if (response.status === 401 && localStorage.getItem('admin-refresh-token') || response.statusCode === 401) {
+  async (error) => {
+    const { config, response } = error
+    console.log(response)
+    if (response.status === 401 || response.statusCode === 401) {
       try {
         const res = await axios({
           method: 'put',
@@ -27,23 +27,24 @@ instance.interceptors.response.use(
           data: {
             refresh_token: localStorage.getItem('admin-refresh-token'),
           },
-        });
-        const { access_token, refresh_token } = res.data;
+        })
+        const { access_token, refresh_token } = res.data
 
-        localStorage.setItem('admin-access-token', access_token);
-        localStorage.setItem('admin-refresh-token', refresh_token);
-        config.headers.Authorization = `Bearer ${access_token}`;
+        localStorage.setItem('admin-access-token', access_token)
+        localStorage.setItem('admin-refresh-token', refresh_token)
+        config.headers.Authorization = `Bearer ${access_token}`
 
-        return axios(config);
+        return axios(config)
       } catch (err: any) {
         if (err.response.status === 401) {
-          alert('다시 로그인해주세요.');
-          window.location.href = '/login';
-          localStorage.clear();
+          console.log(err.response.status)
+          alert('다시 로그인해주세요.')
+          window.location.href = '/login'
+          localStorage.clear()
         }
       }
     }
-    return Promise.reject(error);
-  },
-);
-export default instance;
+    return Promise.reject(error)
+  }
+)
+export default instance
