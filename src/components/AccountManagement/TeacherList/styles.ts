0b28c27d51f8d74c363ac0d9c86cd2bf39ab2a95
oajh04 <cloudtubekr@naver.com>
2022-07-12 21:05:55 +0900

    ⚡️ :: style

--- src/components/AccountManagement/TeacherList/styles.ts
+++ src/components/AccountManagement/TeacherList/styles.ts
@@ -1,32 +1,37 @@
-import styled from '@emotion/styled';
-import { color } from '../../../style/color';
+import styled from '@emotion/styled'
+import { color } from '../../../style/color'
 
 export const TeacherListWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 20%;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+  span {
+    font-size: 22px;
     display: flex;
-    flex-direction: column;
-    width: 20%;
-    height: 100%;
-    align-items: center;
-    justify-content: center;
-    span {
-        font-size: 22px;
-    }
+  }
+  div {
+    margin: 0 5px;
+    cursor: pointer;
+  }
 `
 
 export const TeacherListBox = styled.div`
-    text-align: center;
-    width: 100%;
-    height: 450px;
-    overflow: auto;
+  text-align: center;
+  width: 100%;
+  height: 450px;
+  overflow: auto;
 `
 
-export const Teacher = styled.p<{isActive: boolean}>`
-    font-size: 20px;
-    cursor: pointer;
-    &:hover{
-        color: ${color.orange};
-    }
-    ${(props) =>
+export const Teacher = styled.p<{ isActive: boolean }>`
+  font-size: 20px;
+  cursor: pointer;
+  &:hover {
+    color: ${color.orange};
+  }
+  ${(props) =>
     props.isActive &&
     `
         width: 150px;
@@ -39,5 +44,4 @@ export const Teacher = styled.p<{isActive: boolean}>`
             color: white;
         }
     `}
-    
 `
